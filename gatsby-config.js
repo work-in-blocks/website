bbaa0633271fa9d4b80f682bd2b131dc65c18115
Angel Lacret <alacret@gmail.com>
2018-10-11 10:05:19 -0400

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: 'gatsby-plugin-typography',
       options: {
